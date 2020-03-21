b7c66a3be12bb0b7c74f371b9da2a179afc813fc
jainigo <jainigo@profile.es>
2020-03-21 12:49:10 +0100

    nocdi

--- src/main/java/org/codependent/ExampleResource.java
+++ src/main/java/org/codependent/ExampleResource.java
@@ -2,7 +2,6 @@ package org.codependent;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,8 +11,8 @@ public class ExampleResource {
 
     private final RestClient restClient;
 
-    public ExampleResource(RestClient restClient){
-        this.restClient = restClient;
+    public ExampleResource(){
+        this.restClient = new RestClient();
     }
 
     @GET
--- src/main/java/org/codependent/RestClient.java
+++ src/main/java/org/codependent/RestClient.java
@@ -2,11 +2,9 @@ package org.codependent;
 
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.core.Response;
 
-@ApplicationScoped
 public class RestClient {
 
     private final Client httpClient;
